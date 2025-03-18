754b656d1ac7939db0eaea9a9a85577391f8a711
Evilbob <50513774+masoud-masori@users.noreply.github.com>
2025-03-18 08:03:51 -0400

    Updated files - 2025-03-18 08:03:51

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./frontend/src/**/*.{js,ts,jsx,tsx}", // Adjust this path based on your folder structure
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/line-clamp"),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 };
