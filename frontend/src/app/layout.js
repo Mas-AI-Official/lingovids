9b4b5522273f62807fa13d84bf1d78e924573cae
Evilbob <50513774+masoud-masori@users.noreply.github.com>
2025-03-20 15:47:27 -0400

    Updated auth system, promo code logic, and deployment fixes

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,13 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.metadata = void 0;
-exports.default = RootLayout;
-const jsx_runtime_1 = require("react/jsx-runtime");
-require("./globals.css");
-exports.metadata = {
-    title: 'Lingovids MAS-AI',
-    description: 'Translate and Dub Videos with AI',
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "LingoVids",
+  description: "AI-powered video translations",
 };
-function RootLayout({ children }) {
-    return ((0, jsx_runtime_1.jsx)("html", { lang: 'en', children: (0, jsx_runtime_1.jsx)("body", { children: children }) }));
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
